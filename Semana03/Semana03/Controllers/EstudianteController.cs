bdb6da50b533f026de7782d184a45106da670e68
Adrián Peraza Baldizón <101289832+adrix-10@users.noreply.github.com>
2023-09-27 20:52:52 -0600

    Proyecto Semana 3 Final

---
+++ Semana03/Semana03/Controllers/EstudianteController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Semana03.Models;
+
+namespace Semana03.Controllers
+{
+    public class EstudianteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CrearEstudiante(Estudiante estudiante) 
+        {
+            ViewBag.Titulo = "Información del Estudiante";
+
+            return View("Detalle", estudiante);
+        }
+    }
+}
---
+++ Semana03/Semana03/Models/Estudiante.cs
@@ -0,0 +1,10 @@
+﻿namespace Semana03.Models
+{
+    public class Estudiante
+    {
+        public String Nombre { get; set; }
+        public String Apellido { get; set; }
+        public int Edad { get; set; }
+        public String Carrera { get; set; }
+    }
+}
